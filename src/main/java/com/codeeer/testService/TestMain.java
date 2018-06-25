9f53617afe114d46f3a3760536b817b656e2c93f
codeeer <omer_olcay25@hotmail.com>
2018-06-25 23:52:04 +0300

    StartUp

---
+++ src/main/java/com/codeeer/testService/TestMain.java
@@ -0,0 +1,9 @@
+package com.codeeer.testService;
+
+public class TestMain {
+
+    public static void main(String[] Args){
+        System.out.println("Omer");
+        System.out.println("Omer");
+    }
+}
